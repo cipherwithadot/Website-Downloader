def27067315c589989041773c7c85f5ce38b13f5
Syntax <88819188+PndaBoi@users.noreply.github.com>
2021-11-29 17:27:15 +0530

    :)

---
+++ index.js
@@ -0,0 +1,24 @@
+// Downloads all the crawlable files of example.com
+
+// The files are saved in the same structure as the structure of the website, by using the `bySiteStructure` filenameGenerator
+
+// Links to other websites are filtered out by the urlFilter
+
+const scrape = require('website-scraper');
+const websiteUrl = 'website url here';
+
+scrape({
+    urls: [websiteUrl],
+    urlFilter: function (url) {
+        return url.indexOf(websiteUrl) === 0;
+    },
+    recursive: true,
+    maxDepth: 50,
+    prettifyUrls: true,
+    filenameGenerator: 'bySiteStructure',
+    directory: './node-website'
+}).then((data) => {
+    console.log("Entire website succesfully downloaded");
+}).catch((err) => {
+    console.log("An error ocurred", err);
+});
